f427ffb0a0334177a96b29fde1f3d67f7b6ff150
Paška Stanislav <stanislav.paska@mim.sk>
2018-01-23 15:38:30 +0100

    Added compatibility with Adminer 4.4 Content Security Policy

--- floatThead.php
+++ floatThead.php
@@ -10,17 +10,17 @@ class AdminerFloatThead {
 	 * @param string $pathToFloatThead Path to floatThead js library. Can be url or filesystem relative path related to the adminer directory.
 	 */
 	public function __construct($pathToJquery='https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js',
-		$pathToFloatThead='https://cdnjs.cloudflare.com/ajax/libs/floatthead/2.0.3/jquery.floatThead.min.js') {
+			$pathToFloatThead='https://cdnjs.cloudflare.com/ajax/libs/floatthead/2.0.3/jquery.floatThead.min.js') {
 		$this->pathToJquery = $pathToJquery;
 		$this->pathToFloatThead = $pathToFloatThead;
 	}
 
 	public function head() {
 		if ($this->pathToJquery) {
-			echo '<script src="'.h($this->pathToJquery).'"></script>';
+			echo '<script'.nonce().' src="'.h($this->pathToJquery).'"></script>';
 		}
-		echo '<script src="'.h($this->pathToFloatThead).'"></script>';
-		echo '<script>$(document).ready(function() { $(\'#content table\').first().floatThead(); });</script>';
+		echo '<script'.nonce().' src="'.h($this->pathToFloatThead).'"></script>';
+		echo '<script'.nonce().'>$(document).ready(function() { $(\'#content table\').first().floatThead(); });</script>';
 		echo '<style type="text/css">.floatThead-container { overflow: visible !important; }</style>';
 	}
 }
